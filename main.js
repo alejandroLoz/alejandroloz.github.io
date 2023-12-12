97d8ce288e0146f70028c45ffbf27dd8aac36089
Alejandro <alejandro@customily.com>
2023-12-12 13:55:20 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var input = document.createElement("input");
 input.id = "clear-fields";
 input.type = "button";
 input.value = "clear fields"; 
-input.className = "product-form__submit button button--full-width button--secondary"
+input.className = "product-form__submit button button--full-width button--prmary"
 input.disabled = "true";
 existingButton.parentNode.insertBefore(input, existingButton.nextSibling);
 
