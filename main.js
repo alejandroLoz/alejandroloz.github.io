0f214cb23683444f9cc06c58d97b94d1da1e5655
Alejandro <alejandro@customily.com>
2023-12-12 12:22:50 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-alert('ahora seeeh');
+var existingButton = document.getElementById('ProductSubmitButton-template--16098713010371__main');
+var input = document.createElement("input");
+input.id = "clear-fields";
+input.type = "button";
+input.value = "clear fields"; 
+input.disabled = "true";
+existingButton.parentNode.insertBefore(input, existingButton.nextSibling);
+
+
+
+document.getElementById('clear-fields').setAttribute("disabled","true");
+
+document.getElementById('custom-name').oninput = () => {  
+  document.getElementById('clear-fields').removeAttribute("disabled");
+}
+
+document.getElementById('custom-file').oninput = () => {
+  document.getElementById('clear-fields').removeAttribute("disabled");
+}
+
+document.getElementById('clear-fields').onclick = () => {
+  document.getElementById('custom-name').value = "";
+  document.getElementById('custom-file').value = "";
+  document.getElementById('clear-fields').setAttribute("disabled","true");
+}
+
