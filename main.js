2703f64dc574d005ccff1b20c5a65b07086c096b
Alejandro <alejandro@customily.com>
2023-12-28 16:09:59 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,13 @@
-console.log('*************loading script from shs script tag******************');
-var existingButton = document.getElementById('custom-name').parentNode;
-var input = document.createElement("input");
-input.id = "clear-fields";
-input.type = "button";
-input.value = "clear fields"; 
-input.className = "product-form__submit button button--full-width button--prmary"
-input.disabled = "true";
-existingButton.parentNode.insertBefore(input, existingButton.nextSibling);
-
-
-
-document.getElementById('clear-fields').setAttribute("disabled","true");
-
-document.getElementById('custom-name').oninput = () => {  
-  document.getElementById('clear-fields').removeAttribute("disabled");
-}
-
-document.getElementById('custom-file').oninput = () => {
-  document.getElementById('clear-fields').removeAttribute("disabled");
-}
-
-document.getElementById('clear-fields').onclick = () => {
-  document.getElementById('custom-name').value = "";
-  document.getElementById('custom-file').value = "";
-  document.getElementById('clear-fields').setAttribute("disabled","true");
-}
+const userInputField = document.getElementById('user-input');
+    const userImage = document.getElementById('image-upload');
+    // Function to clear the input field value
+    function clearInputFields() {
+      userInputField.value = '';
+      userImage.value = '';
+    }
+    // Create a Clear Fields button dynamically
+    const clearButton = document.createElement('button');
+    clearButton.textContent = 'Clear Fields';
+    clearButton.addEventListener('click', clearInputFields);
+    // Append the Clear Fields button after the input field
+    userImage.insertAdjacentElement('afterend', clearButton);
