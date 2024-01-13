3fa116cd835353f07e331e1cb5db7e60df3c2817
Alejandro <alejandro@customily.com>
2024-01-12 18:04:59 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
-
-  checkValues();
+checkValues();
 function checkValues() {
- var htmlActions = "<div class='actions' id='custom-actions'><button style='color:#fff;background-color:#ccc;text-align:center;' id='clear' disabled>Clear fields</button></div>";
-  !document.querySelector('.actions') ? document.querySelector('#custom-fields').innerHTML+=htmlActions : false;
+  var htmlActions =
+    "<div class='actions' id='custom-actions'><button style='color:#fff;background-color:#ccc;text-align:center;' id='clear' disabled>Clear fields</button></div>";
+  !document.querySelector(".actions")
+    ? (document.querySelector("#custom-fields").innerHTML += htmlActions)
+    : false;
 
-  document.querySelector('#clear') && document.querySelector('#clear').addEventListener('click', () => {
-    document.querySelector('#inputText1').value = "";
-    document.querySel
+  document.querySelector("#clear") &&
+    document.querySelector("#clear").addEventListener("click", () => {
+      document.querySelector("#inputText1").value = "";
+      document.querySelector("#inputFile1").value = "";
+      updateButton();
+    });
+
+  document
+    .querySelector("#inputText1")
+    .addEventListener("change", () => updateButton());
+  document
+    .querySelector("#inputFile1")
+    .addEventListener("change", () => updateButton());
+}
+
+function updateButton() {
+  var someFieldHasValue = Array.from(
+    document.querySelectorAll(".custom-inputs .line-input input")
+  ).some((v, i) => v.value !== "");
+  if (someFieldHasValue) {
+    document
+      .querySelector("#clear")
+      .setAttribute("style", "background-color: #000");
+    document.querySelector("#clear").removeAttribute("disabled");
+  } else {
+    document
+      .querySelector("#clear")
+      .setAttribute("style", "background-color: #ccc");
+    document.querySelector("#clear").setAttribute("disabled", true);
+  }
+}
