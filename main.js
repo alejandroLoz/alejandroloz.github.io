e3f5cb2e7c6e33bdecac370cbe762b35a072eddb
Alejandro <alejandro@customily.com>
2024-01-20 15:48:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,49 @@
 (function() {
+
   const customFields = document.getElementById('customFields');
+
   const customTextField = document.getElementById('customText');
+
   const customImageField = document.getElementById('fileInput');
+
   const clearFieldsButton = document.createElement('button');
+
   clearFieldsButton.classList.add('button', 'button--full-width', 'button--secondary');
-  
+
+ 
+
   clearFieldsButton.innerText = 'Clear fields';
+
   customFields.appendChild(clearFieldsButton);
+
   clearFieldsButton.addEventListener('click', () => {
+
     customTextField.value = "";
+
     customImageField.value = "";
+
   })
+
   function makeDisabled() {
-    if (customTextField.value  "" && customImageField.value  "") {
+
+    if (customTextField.value == "" && customImageField.value == "") {
+
       clearFieldsButton.disabled = true;
+
     } else {
+
       clearFieldsButton.disabled = false;
+
     }
+
   }
+
   // Check if fields are empty and disable the button
+
   customTextField.addEventListener('keydown', () => makeDisabled())
+
   customImageField.addEventListener('change', () => makeDisabled())
+
   clearFieldsButton.disabled = true;
+
 })()
